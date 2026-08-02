ca2fd23e9277e4d74f3e14b938579ea33cd5960b
skid1numberone <showmyblood@gmail.com>
2026-08-02 22:46:47 +0500

    Update Library.lua

--- src/Library.lua
+++ src/Library.lua
@@ -1,1 +1,12 @@
+local Library = {}
 
+Library.Windows = {}
+
+function Library:CreateWindow(settings)
+    settings = settings or {}
+    local windowTitle = settings.Title or "Window"
+    print("Creating window: " .. windowTitle)
+    return windowTitle
+end
+
+return Library
