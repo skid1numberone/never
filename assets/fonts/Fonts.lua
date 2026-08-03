bdf3403c2d08f63d4d769cac047c0b002a54bb80
skid1numberone <showmyblood@gmail.com>
2026-08-03 14:12:07 +0500

    Create Fonts.lua

---
+++ assets/fonts/Fonts.lua
@@ -0,0 +1,14 @@
+local Fonts = {}
+
+Fonts.Regular = Enum.Font.Gotham
+Fonts.Medium = Enum.Font.GothamMedium
+Fonts.Semibold = Enum.Font.GothamSemibold
+Fonts.Bold = Enum.Font.GothamBold
+Fonts.Black = Enum.Font.GothamBlack
+
+Fonts.Code = Enum.Font.RobotoMono
+Fonts.Rounded = Enum.Font.FredokaOne
+Fonts.Condensed = Enum.Font.SourceSansPro
+Fonts.Italic = Enum.Font.GothamMediumItalic
+
+return Fonts
